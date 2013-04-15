02bd49cd0ac1defdf7778f611dab907be68e5e09
tmiyachi <t.miyachi0630@gmail.com>
2013-04-16 04:31:32 +0900

    tickerを__all__に追加

--- pymet/metplt/__init__.py
+++ pymet/metplt/__init__.py
@@ -3,12 +3,14 @@ from info import __doc__
 import mybasemap
 import crossplt
 import hovmollerplt
+import ticker
 from mybasemap import *
 from crossplt import *
 from hovmollerplt import *
+from ticker improt :
 
 __all__ = []
 __all__ += mybasemap.__all__
 __all__ += crossplt.__all__
 __all__ += hovmollerplt.__all__
-
+__all__ += ticker.__all__
