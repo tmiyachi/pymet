2b76546cbcf8fd2795c36fd35fe3ca621c0e11db
tmiyachi <t.miyachi0630@gmail.com>
2013-04-19 15:14:18 +0900

    McGridオブジェクトのdims属性を内部で自動変更するように改訂

--- pymet/io/gradsio.py
+++ pymet/io/gradsio.py
@@ -88,7 +88,7 @@ class GradsIO:
         elif fid == -1:
             fid = self.fn
         self.ga('close '+str(fid))
-        self.vars.pop(fid)
+        self.vars.pop(fid-1)
         self.fn = self.fn - 1
     def allclose(self):
         u"""
@@ -97,6 +97,7 @@ class GradsIO:
         self.ga('allclose')
         self.fn = 0
         self._first = True
+        self.vars = []
     def command(self,command_string):
         u"""
         コマンドをGrADSに送る。
@@ -110,6 +111,7 @@ class GradsIO:
         u"""
         次元を指定する。        
         """
+#        qh = self.ga.query('ctlinfo')
         locale.setlocale(locale.LC_ALL,'en_US')
         if isinstance(lon, tuple):
             self.ga('set lon %f %f' % lon)
@@ -135,7 +137,9 @@ class GradsIO:
             self.ga('set e %d %d' % ens)
         elif type(ens) == type(int(1)):
             self.ga('set e %d' % ens)
-            
+        elif ens=='all':
+            self.ga('set e 1 %d' % self.ga.query('file',Quiet=True).ne)
+        
     def get(self, var, fid=None):
         u"""
         指定した変数をMcFieldオブジェクトとして取得する。
@@ -143,23 +147,71 @@ class GradsIO:
         if fid==None:
             for i, fvars in enumerate(self.vars):
                 if var in fvars:
-                    fid = i
+                    fid = i+1
                     break
-        self.ga('set dfile '+str(fid))                
+        self.ga('set dfile '+str(fid))
+        dh = self.ga.query('dims', Quiet=True)
         info = self.ga.coords()
-        lon  = np.asarray(info.lon)
-        lat  = np.asarray(info.lat)
-        lev  = np.asarray(info.lev)
-        time = [datetime.strptime(time, '%HZ%d%b%Y') for time in info.time]
-        dims = info.dims
+        ne, nt, nz, ny, nx = dh.ne, dh.nt, dh.nz, dh.ny, dh.nx
+        e1, e2 = dh.ei
+        t1, t2 = dh.ti
+        z1, z2 = dh.zi
+        
+        lon  = np.asarray(info.lon, dtype=np.float32)
+        lat  = np.asarray(info.lat, dtype=np.float32)
+        lev  = np.asarray(info.lev, dtype=np.float32)
+        time = np.asarray([datetime.strptime(time,'%HZ%d%b%Y') for time in info.time])
+        ens  = np.asarray(info.ens)        
 
-        out  = np.asarray(self.ga.eval(var), dtype=np.float32)
+        out = np.zeros((ne,nt,nz,ny,nx))
+        ## 4次元以上はgradsでは同時に扱えないのでループする
+        ## 少ない次元を優先的にループ
+        if nz == max(ne, nt, nz):
+            try:
+                for i, e in enumerate(range(e1,e2+1)):
+                    self.ga.cmd('set e %d' % e)
+                    for j, t in enumerate(range(t1,t2+1)):
+                        self.ga.cmd('set t %d' % t)
+                        out[i,j,:,:,:]  = np.asarray(self.ga.eval(var), dtype=np.float32).reshape((nz,ny,nx))
+                        self.ga.Writer.flush()
+            except:
+                self.ga.flush()
+                self.ga.setdim(dh)
+                raise
+        elif nt == max(ne, nt, nz):
+            try:
+                for i, e in enumerate(range(e1,e2+1)):
+                    self.ga.cmd('set e %d' % e)
+                    for k, z in enumerate(range(z1,z2+1)):
+                        self.ga.cmd('set z %d' % z)
+                        out[i,:,k,:,:]  = np.asarray(self.ga.eval(var), dtype=np.float32).reshape((nt,ny,nx))
+                        self.ga.Writer.flush()
+            except:
+                self.ga.flush()
+                self.ga.setdim(dh)
+                raise
+        else:
+            try:
+                for j, t in enumerate(range(t1,t2+1)):
+                    self.ga.cmd('set t %d' % t)
+                    for k, z in enumerate(range(z1,z2+1)):
+                        self.ga.cmd('set z %d' % z)
+                        out[:,j,k,:,:]  = np.asarray(self.ga.eval(var), dtype=np.float32).reshape((ne,ny,nx))
+                        self.ga.Writer.flush()
+            except:
+                self.ga.flush()
+                self.ga.setdim(dh)
+                raise
+        out  = np.squeeze(out)
         out  = np.ma.array(out, mask=(out==info.undef))
-        out  = out.reshape(info.shape)
+
+        self.ga.flush()
+        self.ga.setdim(dh)
 
         # create McGrid
-        grid = McGrid(name=var, lon=lon, lat=lat, lev=lev, time=time, dims=dims)
+        grid = McGrid(name=var, lon=lon, lat=lat, lev=lev, time=time, ens=ens)
         field = McField(out, name=var, grid=grid, mask=out.mask)
+
         return field
 
         
