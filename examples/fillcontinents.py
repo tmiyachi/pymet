c5461a295e5c38d6fd3206b973fb74ca35c3ae35
miyachi <t.miyachi0630@gmail.com>
2013-04-21 03:03:08 +0900

    add examples

--- examples/fillcontinents.py
+++ examples/fillcontinents.py
@@ -13,7 +13,7 @@ io.setdim(lon=(0,357.5),lat=(-90,90),lev=500,time=datetime(2010,8,1))
 z500 = io.get('hgt')
 lon, lat = z500.grid.latlon()
 
-plt.subplot(1,1,1)
+plt.subplot(2,1,1)
 m = MyBasemap(lon=(0,360.01), lat=(0,90), xlint=60, ylint=30)#, fix_aspect=False)
 m.contour(lon, lat, z500, cint=100)
 m.fillcontinents()
