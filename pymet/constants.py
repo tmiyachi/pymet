9bfac56d46c54e3ac2cd1d8b7ca5d183c0f29940
tmiyachi <t.miyachi0630@gmail.com>
2013-08-10 02:14:44 +0900

    緯度経度座標と局所直交座標の変換メソッドの追加

--- pymet/constants.py
+++ pymet/constants.py
@@ -34,10 +34,12 @@ u"""
 
 ================== ============================================================================================
 ``air_rd``         乾燥空気の気体定数 :math:`R_{d}` [J K^-1 Kg^-1]
+``air_rv``         水蒸気の気体定数   :math:`R_{v}` [J K^-1 Kg^-1]
 ``air_cp``         乾燥空気の定圧比熱 :math:`C_{p}` [J K^-1 Kg^-1]
 ``air_cv``         乾燥空気の定積比熱 :math:`C_{v}` [J K^-1 Kg^-1]
 ``air_gamma``      比熱比 :math:`\gamma=C_{p}/C_{v}`
-``air_kappa``      :math:`R_{d}/C_{p}`
+``air_kappa``      :math:`\kappa=R_{d}/C_{p}`
+``air_eps``        :math:`\epsilon=R_{d}/R_{v}`
 ================== ============================================================================================
 
 -------
@@ -48,7 +50,9 @@ import numpy as _np
 #constant
 
 #math_constants
-pi = _math.pi
+pi  = _math.pi
+d2r = _np.deg2rad   # degree => radian
+r2d = _np.rad2deg   # radian => degree
 
 #Geophysical constants
 earth_radius = 6.371e+6
@@ -58,10 +62,12 @@ earth_gravity = 9.80665
 
 #air_constants
 air_rd = 287.0
+air_rv = 461.0
 air_cp = 1004.0
 air_cv = 717.0
 air_kappa = air_rd/air_cp
 air_gamma = air_cp/air_cv
+air_eps = air_rd/air_rv
 
 #functions for geophysical constants
 def earth_f(lat):
@@ -81,7 +87,7 @@ def earth_f(lat):
 
     **Examples**
      >>> from pymet.constants.mod_const import earth_f
-     >>> earth_f(_np.array([-30., 0., 30.))
+     >>> earth_f(__np.array([-30., 0., 30.))
      array()
     """
     return 2.*earth_omega*_np.sin(pi/180.*lat)
@@ -103,7 +109,31 @@ def earth_beta(lat):
 
     **Examples**
      >>> from pymet.constants.mod_const import earth_beta
-     >>> earth_beta(_np.array([-30., 0., 30.))
+     >>> earth_beta(__np.array([-30., 0., 30.))
      array()
     """
     return 2.*earth_omega/earth_radius*_np.cos(pi/180.*lat)
+
+def dlon2dx(dlon, clat):
+    u"""
+    経度差から局所直交系での実距離を求める。
+    """
+    return d2r(dlon)*earth_radius*_np.cos(d2r(clat))
+
+def dx2dlon(dx, clat):
+    u"""
+    距離から経度差を求める。
+    """
+    return r2d(dx/earth_radius/_np.cos(d2r(clat)))
+
+def dlat2dy(dlat):
+    u"""
+    緯度差から局所直交系での実距離を求める。
+    """
+    return d2r(dlat)*earth_radius
+
+def dy2dlat(dy):
+    u"""
+    距離から緯度差を求める。
+    """
+    return r2d(dy/earth_radius)
